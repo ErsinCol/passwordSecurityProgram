4292d6141f1e57bd2b9ed32ac570898eb8edefac
ERSİN ÇÖL <92751521+ErsinCol@users.noreply.github.com>
2022-03-26 15:47:36 +0300

    Add files via upload

---
+++ KarakterKontrol.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sifreGuvenlikUygulamasi
+{
+    internal class KarakterKontrol
+    {
+        public int bonusPuan = 0;
+        public int buyukSayisi=0;
+        public int kucukSayisi=0;
+        public int rakamSayisi=0;
+        public int sembolSayisi = 0;
+
+        public int sembollerSayisi(string sifre)
+        {
+            char[] semboller = "/*-+,;`.:<>|~¨@€₺é^#'+$%&/{([)]=}?*-_".ToCharArray();
+            for (int i = 0; i < sifre.Length; i++)
+            {
+                for (int j = 0; j < semboller.Length; j++)
+                {
+                    if (sifre[i] == semboller[j])
+                    {
+                        sembolSayisi++;
+                        if (sembolSayisi == 2) { return sembolSayisi; }
+
+                    }
+
+                }
+
+            }
+            return sembolSayisi;
+        }
+        
+
+
+        public int buyukHarfSayisi(string sifre)
+        {
+            char[] buyukHarfler = "ABCÇDEFGĞHIİJKLMNOÖPQRSŞTUÜVWXYZ".ToCharArray();
+
+            for (int i = 0; i < sifre.Length; i++)
+            {
+                for (int j = 0; j < buyukHarfler.Length; j++)
+                {
+                    if (sifre[i] == buyukHarfler[j])
+                    {
+                        buyukSayisi++;
+                        if (buyukSayisi == 2) { return buyukSayisi; }
+
+                    }
+
+                }
+
+            }
+            return buyukSayisi;
+        }
+        public int kucukHarfSayisi(string sifre)
+        {
+            char[] kucukHarfler = "abcçdefghıijklmnoöprsştuüvyzxwq".ToCharArray();
+            for (int i = 0; i < sifre.Length; i++)
+            {
+                for (int j = 0; j < kucukHarfler.Length; j++)
+                {
+                    if (sifre[i] == kucukHarfler[j])
+                    {
+                        kucukSayisi++;
+                        if (kucukSayisi == 2) { return kucukSayisi; }
+                    }
+                }
+
+            }
+            return kucukSayisi;
+        }
+        public int RakamSayisi(string sifre)
+        {
+            char[] rakamlar = "0123456789".ToCharArray();
+            for (int i = 0; i < sifre.Length; i++)
+            {
+                for (int j = 0; j < rakamlar.Length; j++)
+                {
+                    if (sifre[i] == rakamlar[j])
+                    {
+                        rakamSayisi++;
+                        if (rakamSayisi == 2) { return rakamSayisi; }
+                        
+                    }
+                }
+
+            }
+            return rakamSayisi;
+        }
+        
+
+        public int genelPuanHesaplama()
+        {
+            return buyukSayisi * 10 + kucukSayisi * 10 + rakamSayisi * 10+sembolSayisi*10 + bonusPuan;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sifreGuvenlikUygulamasi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               
+                KarakterKontrol kontrol = new KarakterKontrol();
+                Console.WriteLine("Şifre giriniz: ");
+                string password=Console.ReadLine();
+                for (int i = 0; i < password.Length; i++)
+                {
+                    if (password[i] == ' ')
+                    {
+                        Console.WriteLine("Şifre boşluk içermemeli");
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+
+
+                if (password.Length >= 9)
+                {
+
+                    if (password.Length == 9)
+                    {
+                    kontrol.bonusPuan +=10;
+                    }
+                    if ( kontrol.buyukHarfSayisi(password) == 0 || kontrol.kucukHarfSayisi(password) == 0 || kontrol.RakamSayisi(password)==0 || kontrol.sembollerSayisi(password)==0)
+                    {
+                        Console.WriteLine("Şifre bir rakam,büyük harf ve küçük harf içermelidir. Tekrar Deneyiniz...");
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Şifre 9 karakterden daha kısa uzunlukta olamaz...");
+                    Console.ReadLine();
+                    return;
+                }
+
+            if (kontrol.genelPuanHesaplama() < 70)
+            {
+                Console.WriteLine("Şifre zayıf, tekrar deneyiniz.");
+            }
+            else if (kontrol.genelPuanHesaplama() >= 70 && kontrol.genelPuanHesaplama() < 90)
+            {
+                Console.WriteLine("Şifre olağan, kabul edildi.");
+            }
+            else
+            {
+                Console.WriteLine("Şifre çok güçlü, kabul edildi");
+            }
+
+            Console.WriteLine("büyük harf: " + kontrol.buyukSayisi + " kucuk harf: " + kontrol.kucukSayisi + " rakam sayısı: " + kontrol.rakamSayisi+ " sembol sayısı: "+ kontrol.sembolSayisi);
+            Console.ReadKey();
+        }
+    }
+}
+    
